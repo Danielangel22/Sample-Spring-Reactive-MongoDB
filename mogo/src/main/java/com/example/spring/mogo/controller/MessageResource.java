609416d353a20102b323547ca63b82e9088d638c
daniel.angel1 <daniel.angel@elaniin.com>
2021-02-13 13:03:11 -0600

    :100:  update controller

--- mogo/src/main/java/com/example/spring/mogo/controller/MessageResource.java
+++ mogo/src/main/java/com/example/spring/mogo/controller/MessageResource.java
@@ -3,14 +3,12 @@ package com.example.spring.mogo.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,7 +19,6 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RestController
-///@CrossOrigin(origins = "*", allowCredentials = "true")
 public class MessageResource {
 
 	@Autowired
@@ -47,7 +44,7 @@ public class MessageResource {
 
 	}
 
-	@GetMapping("/messages/{threadId}/bythreadId")
+	@GetMapping("/messages/thread/{threadId}")
 	private Flux<Message> findAllByThreadId(@PathVariable("threadId") String threadId) {
 		return this.messageService.findByThreadId(threadId);
 	}
--- mogo/src/main/java/com/example/spring/mogo/service/Mes.java
+++
@@ -1,5 +0,0 @@
-package com.example.spring.mogo.service;
-
-public class Mes {
-
-}
